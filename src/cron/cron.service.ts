4e701c073c9f0b5faecfa43e949c4cb9c40ad1f8
NikBauer34 <nik.bauer.09@mail.ru>
2024-10-06 12:43:47 +0300

    \zc

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -7,9 +7,12 @@ export class CronService {
   addInterval(name: string, milliseconds: number, callback: () => Promise<void>) {
     const interval = setInterval(callback, milliseconds)
     this.schedulerRegistry.addInterval(name, interval)
+    const intervals = this.schedulerRegistry.getIntervals();
+    console.log(intervals)
   }
   deleteInterval(name: string) {
     const intervals = this.schedulerRegistry.getIntervals();
+    console.log(intervals)
     let isCronExists = false
     for(let el of intervals) {
       if (el == name) {
